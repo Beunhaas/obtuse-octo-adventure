a4b5da881264d6276f867c7e9d6e4b0f22bc7d15
unknown <856595@L0123277.ee.intern>
2015-11-06 16:25:23 +0100

    2e commit

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Hello world! :D");
+console.log("Enter name: ");
+var name = "user";
+console.log("Hello, " + name + " I want to play a little game >:D");
+
+gallows = new Array("--------\n|      |\n|\n|\n|\n|\n=====",
+"--------\n|      O\n|\n|\n|\n|\n=====",
+"--------\n|      O\n|      |\n|\n|\n|\n=====",
+"--------\n|      O\n|     \\|\n|\n|\n|\n=====",
+"--------\n|      O\n|     \\|/\n|\n|\n|\n=====",
+"--------\n|      O\n|     \\|/\n|      |\n|\n|\n=====",
+"--------\n|      O\n|     \\|/\n|      |\n|     /\n|\n=====",
+"--------\n|      O\n|     \\|/\n|      |\n|     / \\\n|\n=====");
+
+console.log(gallows[gallows.length-1]);
+
